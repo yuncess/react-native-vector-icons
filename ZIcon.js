612ff5b06de951755d4e757f6f39151aa6fb4f9c
yuncess <yuncess@163.com>
2018-01-26 16:22:24 +0800

    first

---
+++ ZIcon.js
@@ -0,0 +1,18 @@
+/**
+ * ZIcon icon set component.
+ * Usage: <ZIcon name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/ZIcon.json';
+
+const iconSet = createIconSet(glyphMap, 'zIcon', 'ZIcon.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
